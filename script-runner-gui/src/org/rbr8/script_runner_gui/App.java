31b877f54fffafe5754ae2c9149eedf36dff46d5
rbr <bencsr@freemail.hu>
2013-12-22 18:57:53 +0100

    Move to FXML.

--- script-runner-gui/src/org/rbr8/script_runner_gui/App.java
+++ script-runner-gui/src/org/rbr8/script_runner_gui/App.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.rbr8.script_runner_gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,27 +16,16 @@ import javafx.stage.Stage;
  * @author robert
  */
 public class App extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+
+        Scene scene = new Scene(root);
+
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setTitle("Script runner");
     }
 
     /**
@@ -53,5 +39,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
